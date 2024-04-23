88f8b25becc410e2eb8017f58dcc7a07d380ce41
fadgadahababashirdev <fadgadahababashirdev@gmail.com>
2024-04-23 12:30:50 +0200

    commit

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,23 +1,21 @@
 const express = require("express")
 const testPdf = require("../service/testPdf")
 const {getShop} = require("../controllers/shopOwner")
+const {buildPDF} = require("../service/pdf-service")
 const router = express.Router()
-  
-router.get("/report" , async(req,res ,next)=>{
-   try {
-    const shopp = await getShop()
-    const stream = res.writeHead(200 ,{
-        "Content-Type":"application/pdf",
-        "Content-Disposition":"attachment;filename=report.pdf"
-    })
 
-    testPdf.buildPDF(
-        shopp.shops ,
-        (chunk)=>stream.write(chunk),
-        ()=>stream.end()
-    )
-   } catch (error) {
-    console.log(error)
-   }
+router.get("/pdf" , async(req,res)=>{
+    try {
+        const shops = await getShop()
+        const users = await buildPDF(shops)
+
+        res.setHeader(`Content-Type:application/pdf`)
+        res.setHeader(`Content-Disposition:attachment ; filename=report.pdf`)
+        return users
+    } catch (error) {
+        res.status(500).json({status:"failed" , message:error.message})
+    }
 })
+  
+
 module.exports = router
